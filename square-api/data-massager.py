b195ac5a34c1c9eae0c532780edf864fe0843279
mongoben <benp@mongodb.com>
2024-06-30 19:19:21 -0700

    reading from square orders collection and iterating through entire collection of documents

--- square-api/data-massager.py
+++ square-api/data-massager.py
@@ -1,13 +1,25 @@
 import pymongo
 import json
 
+# ---------- functions start here ---------- #
+def readAndProcess(db, col):
+	cursor = col.find({}).limit(10)
+	for document in cursor:
+		print(document)
+
+
+
+# ---------- script starts here ---------- #
 f = open('../../atlas-creds/atlas-creds.json')
 pData = json.load(f)
 
 conn_string = pData["bsri-connection-string"]
 
 client = pymongo.MongoClient(conn_string)
-db = client.test_db
+square_db = client.square
+orders_col = square_db.orders
 
-db.test_collection.insert_one({"hello":"world"})
+# db.orders_col.insert_one({"hello":"world"})
+
+readAndProcess(square_db, orders_col)
 
