e4c4bf00b8c686b575a3b336eb33e62a4cbd7354
mongoben <benp@mongodb.com>
2024-07-16 17:03:01 -0700

    basket massager to insert many and original data to pull clean data from bsri source

--- square-api/original-data.py
+++ square-api/original-data.py
@@ -1,6 +1,5 @@
 import pymongo
 import json
-import re
 from sentence_transformers import SentenceTransformer, util
 
 
@@ -38,61 +37,36 @@ def readAndProcessDocument(document):
 	new_doc["basket"] = new_string
 	return new_doc
 
-def readAndProcessBasket(document):
-	basket = document["basket"]
-	txt = ""
-	txt = re.search("B - ", basket)
-	new_txt = ""
-	if txt != None:
-		new_txt = basket.replace("B - ", "Burrito ")
-		# print(new_txt)
-	if new_txt != "":
-		document["basket"] = new_txt
-
-	return document
-
 
 # ---------- script starts here ---------- #
 f = open('../../atlas-creds/atlas-creds.json')
 pData = json.load(f)
 
-# bsri_string = pData["bsri-connection-string"]
+bsri_string = pData["bsri-connection-string"]
 mdb_string = pData["mdb-connection-string"]
 
 
-# bsri_client = pymongo.MongoClient(bsri_string)
-# square_db = bsri_client.square
-# orders_col = square_db.orders
+bsri_client = pymongo.MongoClient(bsri_string)
+square_db = bsri_client.square
+orders_col = square_db.orders
 
 mdb_client = pymongo.MongoClient(mdb_string)
 retail_demo_db = mdb_client.retail
-orders_demo_col = retail_demo_db.orders
-
-orders_burrito_col = retail_demo_db.orders_updated_baskets
+orders_original_col = retail_demo_db.orders_original
 
 # model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-doc_array = []
-counter = 0
-
-for doc in orders_demo_col.find({}):
+for doc in orders_col.find({}):
 	# w = orders_demo_col.insert_one(readAndProcessDocument(doc))
 	# print(w.inserted_id)
-	new_doc = readAndProcessBasket(doc)
-	doc_array.append(new_doc)
-	counter += 1
-	if counter > 999:
-		w = orders_burrito_col.insert_many(doc_array)
-		print(w)
-		doc_array = []
-		counter = 0
+	# new_doc = readAndProcessDocument(doc)
 	# print(new_doc)
 	# basket_vector = model.encode(new_doc["basket"]).tolist()
 
 	# result_doc['sentence'] = doc
 	# new_doc['vector_embedding'] = basket_vector
-	# print(new_doc)
-	# w = orders_burrito_col.insert_one(new_doc)
-	# print(w.inserted_id)
+	print(doc["created_at"])
+	w = orders_original_col.insert_one(doc)
+	print(w.inserted_id)
 	# print(orders_demo_col.find_one({"_id":w.inserted_id}))
 
