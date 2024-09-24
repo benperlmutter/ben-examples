b240ea3cf11a8c71ec663131aefbe33d4c76b9d7
mongoben <benp@mongodb.com>
2024-09-24 11:42:59 -0700

    upload document loop implemented

--- time-series-and-aggregations/load_time_series_data.py
+++ time-series-and-aggregations/load_time_series_data.py
@@ -15,19 +15,35 @@ mdb_conn_string = pData["mdb-connection-string"]
 # mdb_client = pymongo.MongoClient(mdb_conn_string, readPreference='secondaryPreferred')
 mdb_client = pymongo.MongoClient(mdb_conn_string)
 transactions_db = mdb_client.transactions
-transactions_db.create_collection('txns', timeseries={ 'timeField': 'timestamp', 'metaField': 'metadata' })
+
+# create time series collection if necessary
+# transactions_db.create_collection('txns', timeseries={ 'timeField': 'timestamp', 'metaField': 'metadata' })
+
 txn_coll = transactions_db.txns
 
 # generate documents
+counter = 0
+total_counter = 0
+total_docs_number = 9999
+doc_array = []
 
-doc = {
-	"timestamp": datetime(random.randint(2010, 2021), random.randint(1, 12), random.randint(1, 28)),
-	"metadata": {
-	"item":"box of cereal",
-	"location": "pleasanton",
-	"loyalty_number":1234,
-	"SKU": 4234239845
+while total_counter < total_docs_number:
+	counter += 1
+	total_counter += 1
+	doc = {
+		"timestamp": datetime(random.randint(2010, 2021), random.randint(1, 12), random.randint(1, 28)),
+		"metadata": {
+		"item":"box of cereal",
+		"location": "pleasanton",
+		"loyalty_number":1234,
+		"SKU": 4234239845
+		}
 	}
-}
+	doc_array.append(doc)
+	if len(doc_array) > 999:
+		w = txn_coll.insert_many(doc_array)
+		doc_array = []
+		counter = 0
+	elif total_counter == total_docs_number:
+		w = txn_coll.insert_many(doc_array)
 
-w = txn_coll.insert_one(doc)
