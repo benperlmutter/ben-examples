e9b762b7269b4b48a991315a0188b793d6cae365
mongoben <benp@mongodb.com>
2024-10-02 08:46:31 -0700

    water time series data writing to atlas

--- time-series-and-aggregations/load_time_series_data.py
+++ time-series-and-aggregations/load_time_series_data.py
@@ -17,26 +17,33 @@ mdb_client = pymongo.MongoClient(mdb_conn_string)
 transactions_db = mdb_client.transactions
 
 # create time series collection if necessary
-# transactions_db.create_collection('txns', timeseries={ 'timeField': 'timestamp', 'metaField': 'metadata' })
+transactions_db.create_collection('txns', timeseries={ 'timeField': 'timestamp', 'metaField': 'metadata' })
 
 txn_coll = transactions_db.txns
+
+item_list = ["box of cereal","sodapop","loaf of bread","chips","salsa","milk","eggs","whole chicken","salmon fillet"]
+location_list = ["ocean beach","castro","marina","western addition","andronicos on clement"]
 
 # generate documents
 counter = 0
 total_counter = 0
-total_docs_number = 9999
+total_docs_number = 999999
 doc_array = []
 
 while total_counter < total_docs_number:
 	counter += 1
 	total_counter += 1
+	item_number = random.randint(0, len(item_list)-1)
+	location_number = random.randint(0, len(location_list)-1)
+	item_price = round(random.uniform(1, 10), 2)
 	doc = {
 		"timestamp": datetime(random.randint(2010, 2021), random.randint(1, 12), random.randint(1, 28)),
 		"metadata": {
-		"item":"box of cereal",
-		"location": "pleasanton",
-		"loyalty_number":1234,
-		"SKU": 4234239845
+		"item":item_list[item_number],
+		"price": item_price,
+		"location": location_list[location_number],
+		"loyalty_number":random.randint(9999, 99999),
+		"SKU": random.randint(9999999, 99999999)
 		}
 	}
 	doc_array.append(doc)
---
+++ time-series-and-aggregations/water_time_series.py
@@ -0,0 +1,57 @@
+import pymongo
+import json
+from datetime import datetime
+import random
+
+# ---------- functions start here ---------- #
+
+
+# ---------- script starts here ---------- #
+f = open('../../atlas-creds/atlas-creds.json')
+pData = json.load(f)
+
+mdb_conn_string = pData["mdb-connection-string"]
+
+# mdb_client = pymongo.MongoClient(mdb_conn_string, readPreference='secondaryPreferred')
+mdb_client = pymongo.MongoClient(mdb_conn_string)
+water_data_db = mdb_client.water_data
+
+# create time series collection if necessary
+# water_data_db.create_collection('water_metrics', timeseries={ 'timeField': 'timestamp', 'metaField': 'metadata' })
+
+# water_data_coll = water_data_db.water_metrics
+
+data_point_type = ["flow_meter", "pressure_sensor", "temp_sensor", "leak_sensor", "maintenance_event"]
+metric_type = ["flow_rate_liters_per_minute", "pressure_psi", "temperature_celsius", "duration_minutes", "leak_volume_liters"]
+location_list = ["Main Pipeline Section A", "Main Pipeline Section B", "Resevoir Inlet", "Treatment Plant", "Pump Station", "Distribution Line A"]
+
+# generate documents
+counter = 0
+total_counter = 0
+total_docs_number = 9999
+doc_array = []
+
+while total_counter < total_docs_number:
+	counter += 1
+	total_counter += 1
+	data_point_number = random.randint(0, len(data_point_type)-1)
+	location_number = random.randint(0, len(location_list)-1)
+	doc = {
+		"timestamp": datetime(random.randint(2010, 2021), random.randint(1, 12), random.randint(1, 28), hour=random.randint(1, 23), minute=random.randint(1, 59), second=random.randint(1, 59), microsecond=random.randint(1, 999999)),
+		"metadata": {
+		"sensor_id":data_point_type[data_point_number]+"_"+str(total_counter%20),
+		"metric": metric_type[data_point_number],
+		"metric_value": random.randint(1, 100),
+		"location": location_list[location_number],
+		}
+	}
+	print(doc["metadata"]["sensor_id"])
+	# doc_array.append(doc)
+	# if len(doc_array) > 999:
+	# 	w = water_data_coll.insert_many(doc_array)
+	# 	doc_array = []
+	# 	counter = 0
+	# 	print(total_counter)
+	# elif total_counter == total_docs_number:
+	# 	w = water_data_coll.insert_many(doc_array)
+
